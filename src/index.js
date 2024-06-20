c2924b86cacf449416c477f122ab47f8d533d29d
Gaston Herrlein <herrleingaston@gmail.com>
2024-06-21 01:33:02 +0200

    style: se modifican los imports de index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-import { configureClient } from "./api/client";
-import storage from "./utils/storage";
-import "./index.css";
-import App from "./components/app";
-
 import { Provider } from "react-redux";
+import App from "./components/app";
 import configureStore from "./store";
+import storage from "./utils/storage";
+import { configureClient } from "./api/client";
+import "./index.css";
 
 const accessToken = storage.get("auth");
 configureClient({ accessToken });
@@ -25,30 +22,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-/**
-----------------------------------------------import { setAuthorizationHeader } from './api/client';
-
-
-import configureStore from './store';
-
-const accessToken = storage.get('auth');
-if (accessToken) {
-  setAuthorizationHeader(accessToken);
-}
-
-const router = createBrowserRouter([{ path: '*', element: <App /> }]);
-const store = configureStore({ auth: !!accessToken }, { router });
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
-    </ErrorBoundary>
-  </React.StrictMode>,
-);
-
- */
